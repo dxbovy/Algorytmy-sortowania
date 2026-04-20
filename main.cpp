0215c850db9ef2dfc129a50a0f90bc3bee28a67f
Jan Dębowy <jasiu.debowy@gmail.com>
2026-04-20 13:01:34 +0200

    Dodano do pliku main.cpp funkcjonalność testów

--- main.cpp
+++ main.cpp
@@ -1,20 +1,61 @@
 #include "introsort.hpp"
 #include "mergesort.hpp"
 #include "quicksort.hpp"
+#include <algorithm>
+#include <chrono>
 #include <iostream>
+#include <string>
 using namespace std;
+using namespace std::chrono;
+
+struct enquiry {
+  int numOfElements;
+  double sortedPercentage;
+  bool sortedBackwards;
+};
+
+template <typename T> bool isSorted(T tablica) {
+  for (int i = 0; i < sizeof(tablica) / sizeof(T); i++) {
+    if (tablica[i] > tablica[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+template <typename T>
+void fillArray(T *tablica, int size, double sortedPercentage = 0,
+               bool sortedBackwards) {
+  for (int i = 0; i < size; i++) {
+    tablica[i] = rand() * rand();
+  }
+  if (sortedBackwards == true) {
+    T temp;
+    sort(tablica, tablica + (size - 1));
+    for (int i = 0; i < size / 2; i++) {
+      temp = tablica[i];
+      tablica[i] = tablica[size - 1 - i];
+      tablica[size - 1 - i] = temp;
+    }
+  } else {
+    sort(tablica, tablica+((size-1) * (sortedPercentage/100));
+  }
+}
 
 int main() {
-  int tablica[30] = {47, 12, 89, 5,  66, 23, 91, 34, 78, 19,
-                     55, 3,  98, 41, 10, 72, 28, 83, 60, 15,
-                     37, 94, 6,  51, 82, 22, 68, 45, 11, 77};
+  srand(420);
+  int sizes[5] = {10000, 50000, 100000, 50000, 1000000};
+  double percentages[6] = {
+    25,
+  } for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 5; j++) {
+      int tablica[sizes[j]];
+      fillArray<int>(tablica, sizes[j], 0, true);
 
-  Introsort<int> ms;
-
-  ms.sort(tablica, 0, 29);
-
-  for (int i = 0; i < 30; i++) {
-    cout << tablica[i] << " ";
+      for (int k = 0; k < sizes[j]; k++) {
+        cout << tablica[k] << " ";
+      }
+    }
   }
   return 0;
 }
